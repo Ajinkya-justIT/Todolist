955ed1cdb23e43ef09334b9c1f1020fefa7b7143
Taeon183 <ajinkyawadekar0507@gmail.com>
2021-12-23 17:58:37 +0530

    Initial commit

---
+++ src/pages/Help.vue
@@ -0,0 +1,15 @@
+<template>
+  <q-page class="q-pa-lg">
+    <h5 class="q-mt-none">Help</h5>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quam dolor neque facere! Provident id assumenda magni quam exercitationem laborum consequuntur doloribus sint dolores deleniti? Esse ducimus praesentium officia laboriosam deserunt?</p>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum, doloribus. Asperiores temporibus, eaque nulla esse assumenda sunt magnam minima cumque, iusto quam dignissimos. Impedit repellendus animi autem, saepe beatae atque.</p>
+  </q-page>
+</template>
+
+<script>
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+
+})
+</script>
